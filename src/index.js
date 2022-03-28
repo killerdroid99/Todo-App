2b0229566d35f787fd9252b29a93da1744659b34
Aniket Jena <75910246+killerdroid99@users.noreply.github.com>
2022-03-28 11:56:21 +0530

    Added a sample project with todos

--- src/index.js
+++ src/index.js
@@ -17,6 +17,45 @@ window.addEventListener("keydown", (e) => {
 	}
 });
 
+// Set a sample project in the local storage
+localStorage.setItem(
+	"TodoProject",
+	JSON.stringify([
+		{
+			pid: "1",
+			title: "Sample Project",
+			linkedTodoList: [
+				{
+					uuid: "1a",
+					completedStatus: false,
+					task: "this is the task 3",
+					priority: "High",
+					date: "01/01/2020",
+				},
+				{
+					uuid: "1b",
+					completedStatus: false,
+					task: "this is the task 2",
+					priority: "Medium",
+					date: "01/01/2020",
+				},
+				{
+					uuid: "1c",
+					completedStatus: true,
+					task: "this is the task 1",
+					priority: "Low",
+					date: "01/01/2020",
+				},
+			],
+			total: 3,
+			complete: 1,
+			high: 1,
+			med: 1,
+			low: 1,
+		},
+	])
+);
+
 AddProject();
 DeleteProject();
 EditTitle();
